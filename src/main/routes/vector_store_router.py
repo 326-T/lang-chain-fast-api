d4ba84e8be9cea9f1a384c5f906cd51cdeca050e
Mitsuru Takeda <m.t.hagaren1@gmail.com>
2024-08-08 03:59:44 +0900

    [add]bulkに対応

--- src/main/routes/vector_store_router.py
+++ src/main/routes/vector_store_router.py
@@ -5,33 +5,48 @@ from pydantic import BaseModel
 from src.main.persistence.milvus import vector_store
 
 
-class Item(BaseModel):
+class DocReq(BaseModel):
     source: str
     title: str
     content: str
 
 
+class DocRes(BaseModel):
+    page_content: str
+    metadata: dict[str, str | int]
+
+
 vectorStoreRouter = APIRouter()
 
 
-@vectorStoreRouter.post(path="/vector")
-async def read_root(item: Item) -> None:
+@vectorStoreRouter.post(path="/document")
+async def post(doc: DocReq) -> None:
     vector_store.add_documents(
         documents=[
             Document(
-                page_content=item.content,
-                metadata={"source": item.source, "title": item.title},
+                page_content=doc.content,
+                metadata={"source": doc.source, "title": doc.title},
             )
         ]
     )
 
 
-@vectorStoreRouter.get(path="/vector")
-async def read_root(query: str):
-    results: List[Document] = vector_store.similarity_search(query=query)
-    return {
-        "results": [
-            {"page_content": doc.page_content, "metadata": doc.metadata}
-            for doc in results
+@vectorStoreRouter.post(path="/document/bulk")
+async def bulk(docs: List[DocReq]) -> None:
+    vector_store.add_documents(
+        documents=[
+            Document(
+                page_content=doc.content,
+                metadata={"source": doc.source, "title": doc.title},
+            )
+            for doc in docs
         ]
-    }
+    )
+
+
+@vectorStoreRouter.get(path="/document")
+async def index(query: str) -> List[DocRes]:
+    results: List[Document] = vector_store.similarity_search(query=query)
+    return [
+        DocRes(page_content=doc.page_content, metadata=doc.metadata) for doc in results
+    ]
